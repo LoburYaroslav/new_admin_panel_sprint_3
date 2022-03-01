37eb9388f7547c2a746ed36021a008db7a54bb40
Lobur Yaroslav <lobur.iar@yandex.ru>
2022-03-01 23:43:50 +0300

    поправил метод чтения файла из хранилища

--- 01_etl/app/storage/storage.py
+++ 01_etl/app/storage/storage.py
@@ -2,6 +2,8 @@ import abc
 import json
 from typing import Optional
 
+from lib.logger import logger
+
 
 class BaseStorage:
     @abc.abstractmethod
@@ -20,15 +22,19 @@ class JsonFileStorage(BaseStorage):
         self.file_path = file_path or './storage.json'
 
     def retrieve_state(self) -> dict:
-        with open(self.file_path) as storage_file:
-            raw_data = storage_file.read()
-            return json.loads(raw_data) if raw_data else {}
+        try:
+            with open(self.file_path) as storage_file:
+                raw_data = storage_file.read()
+                return json.loads(raw_data) if raw_data else {}
+        except FileNotFoundError:
+            logger.warning(
+                f'Не найден файл по пути {self.file_path}. '
+                'В качестве данных возвращен пустой словарь. Файл создастся при записи.'
+            )
+            return {}
 
     def save_state(self, state: dict) -> None:
-        try:
-            old_state = self.retrieve_state()
-        except FileNotFoundError:
-            old_state = {}
+        old_state = self.retrieve_state()
 
         with open(self.file_path, 'w') as storage_file:
             new_state = {**old_state, **state}
