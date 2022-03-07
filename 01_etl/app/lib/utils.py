db21fa56e87980d96d3aa9f7a627a8a92924c1be
Lobur Yaroslav <lobur.iar@yandex.ru>
2022-03-07 16:11:43 +0300

    добавил backoff

--- 01_etl/app/lib/utils.py
+++ 01_etl/app/lib/utils.py
@@ -1,13 +1,19 @@
+import time
 from functools import wraps
+from typing import Tuple, Type, Union
+
+from lib.logger import logger
 
 
 def backoff(
+        exception_or_exceptions: Union[Type[Exception], Tuple[Type[Exception]]],
         start_sleep_time=0.1,
         factor=2,
-        border_sleep_time=10
+        border_sleep_time=10,
 ):
     """
-    Функция для повторного выполнения функции через некоторое время, если возникла ошибка. Использует наивный экспоненциальный рост времени повтора (factor) до граничного времени ожидания (border_sleep_time)
+    Функция для повторного выполнения функции через некоторое время, если возникла ошибка.
+    Использует наивный экспоненциальный рост времени повтора (factor) до граничного времени ожидания (border_sleep_time)
 
     Формула:
         t = start_sleep_time * 2^(n) if t < border_sleep_time
@@ -15,13 +21,28 @@ def backoff(
     :param start_sleep_time: начальное время повтора
     :param factor: во сколько раз нужно увеличить время ожидания
     :param border_sleep_time: граничное время ожидания
+    :param exception_or_exceptions: исключение или исключения, при возникновении которых будем делать ретраи
     :return: результат выполнения функции
     """
+
+    # Если не переданы конкретные исключения по дефолту ловим все.
+    # Рекомендуется передавать конкретные исключения!
+    catch_exceptions = exception_or_exceptions or Exception
 
     def func_wrapper(func):
         @wraps(func)
         def inner(*args, **kwargs):
-            ...
+            retries = 0
+            while True:
+                try:
+                    return func(*args, **kwargs)
+                except catch_exceptions:
+                    logger.exception('An known exception occurred, so retry will be done.')
+                    sleep_time = start_sleep_time + factor ** retries
+                    if sleep_time >= border_sleep_time:
+                        sleep_time = border_sleep_time
+                    time.sleep(sleep_time)
+                    retries += 1
 
         return inner
 
